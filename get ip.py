683598297949a5ebac4887ba1c10796066976c12
i am moein <44437189+moeinsk@users.noreply.github.com>
2019-03-05 04:25:07 -0800

    Update get ip.py

--- get ip.py
+++ get ip.py
@@ -3,7 +3,7 @@ from tkinter import *
 from socket import *
 
 win = Tk()
-win.geometry("155x120")
+win.geometry("170x300")
 win.title("get ip")
 win.resizable(0,0)
 win.configure(background="black")
@@ -14,7 +14,7 @@ def ip():
 		try:
 			l2.configure(text=gethostbyname(ip),fg="green")
 		except:
-			l2.configure(text="name website is false",fg="yellow")
+			l2.configure(text="your website adres is false!!",fg="yellow")
 
 
 l = Label(win , text="writed by moein",fg="yellow",bg="black")
@@ -32,6 +32,43 @@ b.grid()
 
 l2 = Label(win , text="",fg="green",bg="black")
 l2.grid()
+
+def color():
+
+		color_ = rad1.get()
+		if color_ ==1:
+				win.configure(background="red")
+				r.configure(bg="red")
+				r1.configure(bg="red")
+				r2.configure(bg="red")
+		elif color_==2:
+				win.configure(background="blue")
+				r.configure(bg="blue")
+				r1.configure(bg="blue")
+				r2.configure(bg="blue")
+		elif color_==3:
+				win.configure(background="yellow")
+				r.configure(bg="yellow")
+				r1.configure(bg="yellow")
+				r2.configure(bg="yellow")
+
+rad1 = IntVar()
+
+l = Label(win , text="select your background ",fg="red",bg="black")
+l.grid()
+
+
+r = Radiobutton(win , text="red",variable=rad1,value=1,command=color)
+r.grid()
+
+
+r1 = Radiobutton(win , text="blue",variable=rad1,value=2,command=color)
+r1.grid()
+
+
+r2 = Radiobutton(win , text="yellow",variable=rad1,value=3,command=color)
+r2.grid()
+
 
 
 
